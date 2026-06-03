b1acf903b534c63281db0d51d809ef1d7298bc59
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2026-06-03 09:38:23 +0200

    Change `implementation` to `api` for `kompute-core` dependency in `kompute-opengl` module

--- kompute-opengl/build.gradle.kts
+++ kompute-opengl/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation(project(":kompute-core"))
+    api(project(":kompute-core"))
     implementation(libs.lwjgl)
     implementation(libs.lwjgl.glfw)
     implementation(libs.lwjgl.opengl)
