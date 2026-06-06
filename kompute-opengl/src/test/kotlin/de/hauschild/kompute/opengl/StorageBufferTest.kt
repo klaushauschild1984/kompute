f961f2e3ed96dcdc0b228438e68c6c54395fa087
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2026-06-06 07:09:40 +0200

    Remove deprecated `copy.glsl` shader, simplify `OpenGLBackendTest`, and add new tests for `UniformBuffer` and `StorageBuffer`.

---
+++ kompute-opengl/src/test/kotlin/de/hauschild/kompute/opengl/StorageBufferTest.kt
@@ -0,0 +1,43 @@
+package de.hauschild.kompute.opengl
+
+import de.hauschild.kompute.core.ShaderSource.Code
+import de.hauschild.kompute.core.StorageBuffer
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.extension.ExtendWith
+import kotlin.test.Test
+
+@ExtendWith(OpenGLBackendExtension::class)
+class StorageBufferTest {
+    @Test
+    fun `float array`(backend: OpenGLBackend) {
+        val output = StorageBuffer<FloatArray>(1).size(3).asOutput()
+        val result =
+            backend
+                .shader(
+                    Code("""
+#version 430 core
+layout (local_size_x = 1) in;
+
+layout (std430, binding = 0) readonly buffer InputBuffer {
+    float values[];
+} source;
+
+layout (std430, binding = 1) writeonly buffer OutputBuffer {
+    float values[];
+} result;
+
+void main() {
+    result.values[gl_GlobalInvocationID.x] = source.values[gl_GlobalInvocationID.x];
+}
+                    """.trimIndent()),
+                )
+                .data(
+                    StorageBuffer<FloatArray>(0).data(floatArrayOf(1f, 2f, 3f)),
+                    output,
+                )
+                .dispatch(3)
+                .execute()[output]
+
+        assertArrayEquals(floatArrayOf(1f, 2f, 3f), result)
+    }
+}
---
+++ kompute-opengl/src/test/kotlin/de/hauschild/kompute/opengl/UniformBufferTest.kt
@@ -0,0 +1,52 @@
+package de.hauschild.kompute.opengl
+
+import de.hauschild.kompute.core.ShaderData.UniformBuffer
+import de.hauschild.kompute.core.ShaderSource.Code
+import de.hauschild.kompute.core.StorageBuffer
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.extension.ExtendWith
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import kotlin.test.Test
+
+@ExtendWith(OpenGLBackendExtension::class)
+class UniformBufferTest {
+    @Test
+    fun `float array`(backend: OpenGLBackend) {
+        val output = StorageBuffer<FloatArray>(1).size(3).asOutput()
+        val result =
+            backend
+                .shader(
+                    Code("""
+#version 430 core
+layout (local_size_x = 1) in;
+
+layout (std140, binding = 0) uniform Params {
+    float value;
+} params;
+
+layout (std430, binding = 1) writeonly buffer OutputBuffer {
+    float values[];
+} outputBuffer;
+
+void main() {
+    outputBuffer.values[gl_GlobalInvocationID.x] = params.value;
+}
+                    """.trimIndent()),
+                )
+                .data(
+                    UniformBuffer(0).data(
+                        ByteBuffer
+                            .allocate(Float.SIZE_BYTES)
+                            .order(ByteOrder.nativeOrder())
+                            .putFloat(42f)
+                            .array()
+                    ),
+                    output,
+                )
+                .dispatch(3)
+                .execute()[output]
+
+        assertArrayEquals(floatArrayOf(42f, 42f, 42f), result)
+    }
+}
