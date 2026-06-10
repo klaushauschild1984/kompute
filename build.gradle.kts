9abd6576c8a88afd617179e8893db698b371ba5a
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2026-06-10 13:19:02 +0200

    Update CI workflow: fix Jacoco coverage badge generation path and remove unused `jacocoCoreReport` task

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,30 +12,3 @@ repositories {
     mavenCentral()
 }
 
-tasks.register<JacocoReport>("jacocoCoreReport") {
-    dependsOn(
-        ":kompute-core:test",
-        ":kompute-core:jacocoTestReport",
-    )
-
-    executionData.from(
-        project(":kompute-core").layout.buildDirectory.file("jacoco/test.exec"),
-    )
-
-    sourceDirectories.from(
-        project(":kompute-core").file("src/main/kotlin"),
-    )
-
-    classDirectories.from(
-        project(":kompute-core").layout.buildDirectory.dir("classes/kotlin/main"),
-    )
-
-    reports {
-        html.required = true
-        csv.required = true
-    }
-}
-
-tasks.check {
-    dependsOn("jacocoCoreReport")
-}
