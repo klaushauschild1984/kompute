443c9c1753abef06afd884b01db36f39bf1d83d6
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2026-06-11 10:44:18 +0200

    Enable `read-write storage buffer` test by removing @Disabled annotation

--- kompute-opengl/src/test/kotlin/de/hauschild/kompute/opengl/data/OpenGLStorageBufferTest.kt
+++ kompute-opengl/src/test/kotlin/de/hauschild/kompute/opengl/data/OpenGLStorageBufferTest.kt
@@ -5,7 +5,6 @@ import de.hauschild.kompute.core.shader.ShaderSource.Code
 import de.hauschild.kompute.opengl.OpenGLBackendExtension
 import de.hauschild.kompute.opengl.backend.OpenGLBackend
 import org.junit.jupiter.api.Assertions.assertArrayEquals
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -41,7 +40,6 @@ class OpenGLStorageBufferTest {
         }
     }
 
-    @Disabled
     @ParameterizedTest(name = "{0}")
     @MethodSource
     fun `read-write storage buffer`(
