1a3a348796f5399ba3f10cfb9e4185ebb8f0acd9
Klaus Hauschild <klaus.hauschild.1984@gmail.com>
2026-06-11 10:15:56 +0200

    Enable storage buffer tests by removing @Disabled annotation and uncommenting test cases

--- kompute-opengl/src/test/kotlin/de/hauschild/kompute/opengl/data/OpenGLStorageBufferTest.kt
+++ kompute-opengl/src/test/kotlin/de/hauschild/kompute/opengl/data/OpenGLStorageBufferTest.kt
@@ -5,7 +5,6 @@ import de.hauschild.kompute.core.shader.ShaderSource.Code
 import de.hauschild.kompute.opengl.OpenGLBackendExtension
 import de.hauschild.kompute.opengl.backend.OpenGLBackend
 import org.junit.jupiter.api.Assertions.assertArrayEquals
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -41,7 +40,6 @@ class OpenGLStorageBufferTest {
         }
     }
 
-    @Disabled
     @ParameterizedTest(name = "{0}")
     @MethodSource
     fun `read-write storage buffer`(
@@ -101,8 +99,8 @@ class OpenGLStorageBufferTest {
         @JvmStatic
         fun `storage buffer`(): Stream<Arguments> = Stream.of(
             Arguments.of("int", intArrayOf(1, 2, 3), IntArray::class),
-//            Arguments.of("float", floatArrayOf(1f, 2f, 3f), FloatArray::class),
-//            Arguments.of("double", doubleArrayOf(1.0, 2.0, 3.0), DoubleArray::class),
+            Arguments.of("float", floatArrayOf(1f, 2f, 3f), FloatArray::class),
+            Arguments.of("double", doubleArrayOf(1.0, 2.0, 3.0), DoubleArray::class),
         )
 
         @JvmStatic
